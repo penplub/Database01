68654bb2d1bd8eb7cb9f89fc11ffd5513ebeb89e
penplub <71209221+penplub@users.noreply.github.com>
2023-06-29 15:11:21 +0700

    Add files via upload

---
+++ Lab1data/Student.java
@@ -0,0 +1,32 @@
+import javax.print.DocFlavor.STRING;
+public class Student {
+
+    private int year;
+    private int rank_display;
+    private String university ;
+    private double score ;
+    private String link;
+    private String country;
+    private String region;
+    private String logo;
+    public Student(){
+        this.year = -1;
+        this.rank_display = -1;
+        this.university = "";
+        this.score = -1.0;
+        this.link = "";
+        this.country = "";
+        this.region = "";
+        this.logo = "";
+    }
+    
+
+
+
+
+
+
+
+
+    
+}
---
+++ Lab1data/lab1.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+/**
+ * lab1
+ */
+public class lab1 {
+
+    public static void main(String[] args) {
+        
+    }
+}
